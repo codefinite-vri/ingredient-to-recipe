0357bf2f72c47f765b899232e909902063df4aa3
Vritika Naik <codefinite-vri@users.noreply.github.com>
2021-07-31 22:24:48 +0530

    Updated Ingredient Recognition UI

--- src/Components/ImageSearch.js
+++ src/Components/ImageSearch.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import {getImageSearch, postImgbb} from '../api.js'
 import {Button, Row, Col} from 'reactstrap'
+import Alert from '@material-ui/lab/Alert';
 
 class SearchImage extends Component {
     constructor(props) {
@@ -69,41 +70,62 @@ class SearchImage extends Component {
   
     render() {
       return (
+        <>
+        <h2 className="text-center mb-4 border-bottom-red">INGREDIENT RECOGNITION</h2>
         <div>
           <Row>
-            <Col>                          
-              <h1>Select Image</h1>
-              <br></br>
-              <input type="file" name="myImage" onChange={this.onImageChange} />
-              
-              <h4>OR</h4>
-              
-              <input type="text" value={this.state.url} name="imageURL" onChange={this.onUrlChange} />
+            <Col className='col-border'>
+            <center>
+              <h5>Upload Image</h5>
+              <input type="file" className="width-fix" name="myImage" onChange={this.onImageChange} />
+              <br/><br/>
+              <h5>OR</h5>
+              <br/>
+              <h5>Add Hosted Image URL</h5>
+              <input className="size-list" type="text" value={this.state.url} name="imageURL" onChange={this.onUrlChange} />
+              {/* <br/><br/>
+              <h5 className="size-list">NOTE: The values shown are subject to the examples fed to the model and we don't guarantee 100% accuracy. However, you will be able to find the closest option based on the image entered. The options viewed do not restrict to just ingredients.</h5> */}
+              </center>  
             </Col>
             {this.state.url != ''?
-              <Col>
-                <img src={this.state.image} height="200" weight="200"/>
+            <>
+              <Col className='col-border'><center>
+              <div id="border-img">
+              
+                <img src={this.state.image} height="200" weight="200"/></div>
                 <br></br>
-                <Button type="button" onClick={this.onAnalyse}>Analyse</Button>
+                <Button type="button" onClick={this.onAnalyse} className='btn-red' >Analyse</Button></center>
+                </Col>
                 {this.state.tags?
                   <>
-                    <br></br> 
-                    <ol>
-                    <lh>Top 3 contendors!!</lh>
-                    <li>{JSON.parse(this.state.tags).result.tags[0].tag.en + " with confidence: "+ JSON.parse(this.state.tags).result.tags[0].confidence.toFixed(2) +"%"}</li>
-                    <li>{JSON.parse(this.state.tags).result.tags[1].tag.en + " with confidence: "+ JSON.parse(this.state.tags).result.tags[1].confidence.toFixed(2) +"%"}</li>
-                    <li>{JSON.parse(this.state.tags).result.tags[2].tag.en + " with confidence: "+ JSON.parse(this.state.tags).result.tags[2].confidence.toFixed(2) +"%"}</li>
+                    <Col className='col-border'>
+                    <center>
+                    <h2>TOP 3 POSSIBILITIES FOR THE INGREDIENT</h2>
+                    <br/>
+                    <ol className="size-list">                    
+                    <Alert variant="filled"  icon={false} style={{marginBottom:'10px', backgroundColor:'#9B1003'}}>
+                    <li className="uppercase-text">{JSON.parse(this.state.tags).result.tags[0].tag.en + " with confidence: "+ JSON.parse(this.state.tags).result.tags[0].confidence.toFixed(2) +"%"}</li>
+                    </Alert>
+                    <Alert variant="filled" severity="warning" icon={false} style={{marginBottom:'10px' , backgroundColor:'#B90c0a'}}>
+                    <li className="uppercase-text">{JSON.parse(this.state.tags).result.tags[1].tag.en + " with confidence: "+ JSON.parse(this.state.tags).result.tags[1].confidence.toFixed(2) +"%"}</li>
+                    </Alert>
+                    <Alert variant="filled" severity="error" icon={false} style={{marginBottom:'10px', backgroundColor:'#E3242B'}}>
+                    <li className="uppercase-text">{JSON.parse(this.state.tags).result.tags[2].tag.en + " with confidence: "+ JSON.parse(this.state.tags).result.tags[2].confidence.toFixed(2) +"%"}</li>
+                    </Alert>
                     </ol>
+                    </center>
+                    </Col> 
                   </>
                 :
                   <></>
                 }
-              </Col>
+              </>
             :
               <></>  
             } 
           </Row>
         </div>
+        </>
       );
     }
   }
