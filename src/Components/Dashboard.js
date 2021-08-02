347b3f011f573e480f7140668c47c03391f05200
Vritika Naik <codefinite-vri@users.noreply.github.com>
2021-08-02 18:40:34 +0530

    Updated AdminRoute Table

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { makeStyles} from '@material-ui/core/styles';
 import { Card, Alert, Row, Col, Button } from 'react-bootstrap'
-import {  NavItem, NavLink} from 'reactstrap'
 import { Link, useHistory } from 'react-router-dom'
 import { useAuth } from '../Contexts/AuthContext'
 import Paper from '@material-ui/core/Paper';
@@ -15,8 +14,6 @@ import TableRow from '@material-ui/core/TableRow';
 import AddRecipe from './AddRecipe';
 import RecipeCarousel from './RecipeCarousel'
 import { getMyRecipes } from '../api';
-import {Table as BootstrapTable} from 'react-bootstrap';
-import {Container} from 'react-bootstrap';
 
 export function LoginButton() {
     const [error, setError] = useState('')
